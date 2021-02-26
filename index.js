79ecf27aba163d5e67d3b3bac332bb7a34eb9832
Will B <willhbjr@gmail.com>
2021-02-26 02:10:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let variable = 33333;
+let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+//Create an anonymous fucntion and set it equal to a variable.
+let checkNumOrStr = function (variable){
+  if (typeof variable == 'string'){
+    return "ARRR!";
+  };
+  return variable *3;
+};
+
+
+/* Your function should:
+a) If passed a number, return the tripled value.
+b) If passed a string, return the string “ARRR!”
+c) Be sure to test your function before moving on the next part. */
+
+let mappedArray = arr.map(checkNumOrStr);
+
+
+
+/* Use your fuction and the map method to change an array  as follows:
+a) Triple any the numbers.
+b) Replace any strings with “ARRR!”
+c) Print the new array to confirm your work.
+*/
+
+
+console.log(checkNumOrStr(variable));
+console.log(arr);
+console.log(mappedArray);
